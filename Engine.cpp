23c8b9ad8fb3fb1d109bc4add3b08da0e9ed71ef
JackalMode <blackbirdpenny375@gmail.com>
2024-11-07 08:11:59 -0500

    Added the Tetr... need to add gravity and what have you

--- Engine.cpp
+++ Engine.cpp
@@ -1,12 +1,13 @@
 #include <SFML/Graphics.hpp>
-#include "player.h"
 #include "Engine.h"
+#include <cstdlib>
 
 using namespace sf;
 using namespace std;
 
 Engine::Engine() : window(VideoMode(GRID_WIDTH * TILE_SIZE, GRID_HEIGHT * TILE_SIZE), "Tetris") {
     Grid();
+    spawnTetr();
 }
 
 void Engine::Grid(){
@@ -17,10 +18,56 @@ void Engine::Grid(){
     }
 }
 
+void Engine::spawnTetr() {
+    int randomShape = rand() % 7;
+
+    switch(randomShape){
+        case 0:
+            //I-shape
+            currentTetromino.blocks = {{4, 0}, {5, 0}, {6, 0}, {7, 0}};
+            currentTetromino.color = Color::Cyan;
+            break;
+        case 1:
+            //O-shape
+            currentTetromino.blocks = {{4, 0}, {5, 0}, {4, 1}, {5, 1}};
+            currentTetromino.color = Color::Yellow;
+            break;
+        case 2:
+            //S-shape
+            currentTetromino.blocks = {{4, 0}, {5, 0}, {3, 1}, {4, 1}};
+            currentTetromino.color = Color::Red;
+            break;
+        case 3:
+            //Z-shape
+            currentTetromino.blocks = {{3, 0}, {4, 0}, {4, 1}, {5, 1}};
+            currentTetromino.color = Color::Green;
+            break;
+        case 4:
+            //L-shape
+            currentTetromino.blocks = {{3, 0}, {3, 1}, {3, 2}, {4, 2}};
+            currentTetromino.color = Color{255, 165, 0, 255};
+            break;
+        case 5:
+            //J-shape
+            currentTetromino.blocks = {{4, 0}, {4, 1}, {4, 2}, {3, 2}};
+            currentTetromino.color = Color(255, 141, 161, 255);
+            break;
+        case 6:
+            //T-Shape
+            currentTetromino.blocks = {{3, 0}, {4, 0}, {5, 0}, {4, 1}};
+            currentTetromino.color = Color(127, 0, 255, 255);
+            break;
+    }
+
+}
+
 void Engine::run(){
     while(window.isOpen()){
         Event event;
         while(window.pollEvent(event)){
+            if(event.type == Event::Closed){
+                window.close();
+            }
             if(event.type == Event::KeyPressed){
                 if(event.key.code == Keyboard::Escape) {
                     window.close();
@@ -39,13 +86,49 @@ void Engine::render(){
         for (int x = 0; x < GRID_WIDTH; ++x) {
             RectangleShape cell(Vector2f(TILE_SIZE - 1, TILE_SIZE -1));
             cell.setPosition(x * TILE_SIZE, y * TILE_SIZE);
-            cell.setFillColor(grid[y][x] == 0 ? Color::Blue : Color::White);
+            cell.setFillColor(grid[y][x] == 0 ? Color::Black : Color::White);
             window.draw(cell);
         }
     }
+    for(const auto& block : currentTetromino.blocks){
+        RectangleShape cell(Vector2f(TILE_SIZE - 1, TILE_SIZE - 1));
+        cell.setPosition(block.x * TILE_SIZE, block.y * TILE_SIZE);
+        cell.setFillColor(currentTetromino.color);
+        window.draw(cell);
+    }
+    window.setFramerateLimit(15);
     window.display();
 }
 
 void Engine::update(){
-
+    if(Keyboard::isKeyPressed(sf::Keyboard::Left)){
+        bool canMove = true;
+        for(const auto& block : currentTetromino.blocks){
+            int newX = block.x - 1;
+            if(newX < 0 || grid[block.y][newX] != 0){
+                canMove = false;
+                break;
+            }
+        }
+        if(canMove) {
+            for (auto& block : currentTetromino.blocks){
+                block.x -= 1;
+            }
+        }
+    }
+    if(Keyboard::isKeyPressed(Keyboard::Right)){
+        bool canMove = true;
+        for (const auto& block : currentTetromino.blocks) {
+            int newX = block.x + 1;
+            if (newX >= GRID_WIDTH || grid[block.y][newX] != 0) {
+                canMove = false;
+                break;
+            }
+        }
+        if (canMove) {
+            for (auto& block : currentTetromino.blocks) {
+                block.x += 1;
+            }
+        }
+    }
 }
--- Engine.h
+++ Engine.h
@@ -5,11 +5,17 @@
 #ifndef M4OEP_ENGINE_H
 #define M4OEP_ENGINE_H
 #include <SFML/Graphics.hpp>
+#include <vector>
 using namespace sf;
 
 const int GRID_WIDTH = 10;
 const int GRID_HEIGHT = 20;
 const int TILE_SIZE = 30;
+
+struct Tetromino {
+    std::vector<Vector2i> blocks;
+    Color color;
+};
 
 class Engine {
 public:
@@ -18,6 +24,8 @@ public:
 private:
     RenderWindow window;
     int grid[GRID_HEIGHT][GRID_WIDTH];
+    Tetromino currentTetromino;
+    void spawnTetr();
     void Grid();
     void update();
     void render();
