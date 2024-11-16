4f83e3a83815b27cb8a5699c7626db7d4c104064
JackalMode <blackbirdpenny375@gmail.com>
2024-11-16 11:44:39 -0500

    Minor changes

--- util/main.cpp
+++ util/main.cpp
@@ -2,10 +2,6 @@
 // Created by Mitchell Steenbergen on 11/5/2024.
 //
 #include "Engine.h"
-#include <SFML/Graphics.hpp>
-#include <iostream>
-using namespace sf;
-using namespace std;
 
 int main() {
     // Create an instance of the game Engine
